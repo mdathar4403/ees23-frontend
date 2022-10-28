0389c34a0d250bd5cf58a09e44a45a03790305fc
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2022-10-28 18:25:43 +0530

    boileer plate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="title">
+
+      </div>
+      <div className="loading">
+
+      </div>
+      <div className="image"></div>
+      <div className="events"></div>
       
     </div>
   );
