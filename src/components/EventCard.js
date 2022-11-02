e4216c0e5cd9fd19e8a2391083d5e6f17162baa7
mdathar <mdathar4403@gmail.com>
2022-11-02 23:03:05 +0530

    event card

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './EventCard.css';
-import {AiOutlineClose} from 'react-icons/ai'
 
 const paragraph=[
-  "abc","def","ghi"
+  "Udyam is the Annual Technological Festival of the department of Electronics Engineering, IIT (BHU) Varanasi. It brings a broad spectrum of events spanning Data Science, Machine Learning, Digital and Analog Electronics and others to boost the participant's skill sets. Since 2010, Udyam has provided a platform to develop and showcase the technical skills of Indian technocrats. And we are pleased to announce that the grand event is again returning this spring as UDYAM'23!",
+  "Udgam is the Annual Cultural Festival of the department of Electronics Engineering, IIT (BHU) Varanasi. It brings you multifarious events that kindle your creative side and awaken your artistic self. EES introduced Udgam at the start of 2022 to provide a platform for participants to showcase their talents in non-technical fields like fine arts, dance, music, photography, and literature. Udgam is back again, bigger and brighter, and awaiting to witness the vigour its participants would bring to the stage. Get ready to manifest your artistry and let your creative self sparkle!",
+  "Mashal is the sports festival of the department of Electronics Engineering, IIT (BHU) Varanasi. It offers a venue for all significant competitions and sports to foster good sportsmanship among the students. Even though it started last year, the event covers numerous sports, including Marathon, Chess, Valorant, Table Tennis, Basketball, Tennis, Football, Cricket, Badminton, Kabaddi, Athletics, and Tug of War. Mashal unleashes the competitive spirit of the nation's brightest minds and offers a perfect arena for sports enthusiasts to face off against one another. As said by Serena Williams, let's overpower, overtake and overcome!"
 ]
-  
+const heading=[
+  "UDYAM",
+  "UDGAM",
+  "MASHAL"
+]
 
 
 const EventCard = (props) => {
@@ -20,21 +25,12 @@ const EventCard = (props) => {
       <div id = 'card' className="event-detais-card">
         <div className="event-logo-container">
           <img src="./logo192.png" alt="" />
-          <button className='close-btn' onClick={off}><AiOutlineClose/></button>    
+          <a href="#" className="close" onClick={off}></a>
         </div>
-        <h1>UDYAM</h1>
+        <h1>{heading[props.data]}</h1>
         <div className="event-content">
-          <p> 
-          {paragraph[props.data]}
-             {/* Nullcon presents Resume & Career Clinic, a virtual edition of career
-            guidance series to connect individuals with security experts and
-            leaders for providing counseling in resume building, providing
-            interview tips, and general career advice in specific areas of the
-            InfoSec industry. Our community reviewers, who have a humungous
-            amount of combined experience in the security industry, of being
-            interviewers and vetting candidates are all set to put their
-            experiences to work in the Resume & Career Clinic by providing
-            individuals one-on-one feedback and guidance over a virtual session.  */}
+          <p> {paragraph[props.data]}
+             
           </p>
         </div>
       </div>
--- src/components/event.js
+++ src/components/event.js
@@ -2,50 +2,47 @@
 import "./style.css";
 import React, { useState } from "react";
 import EventCard from "./EventCard";
-
 function Event() {
-  const toggle = () => {
-    const card = document.getElementById('card');
-    card.style.display = 'block';
+  const toggle = ()=>{
+    const card= document.getElementById('card');
+    card.style.display='block';
   }
-  const [content, setContent] = useState(0);
+  const[content,setContent] = useState(0);
 
   return (
 
-    <div className="events">
-      <table className="table">
-        <thead>
-          <th>EVENTS</th>
-        </thead>
-        <tbody>
-          <div className="table-body">
-            <a href="#" onClick={ () => {
-              setContent(0);
-              console.log("hi");
-              
-            }}>UDYAM</a>
-            <a href="#" onClick={ () => {
-              setContent(1);
-              console.log("h");
-              
-            }} id="event-dark">
-              UDGAM
-            </a>
-            <a href="#" onClick={ () => {
-              setContent(2);
-              
-            }} id="event-dark2">
-              MASHAL
-            </a>
-          </div>
-        </tbody>
-      </table>
-      {/* <div>events</div>
+  <div className="events">
+    <table className="table">
+      <thead>
+        <th>EVENTS</th>
+      </thead>
+      <tbody>
+        <div className="table-body">
+          <a href="#" onClick={()=>{
+            setContent(0);
+            toggle();
+          }}>UDYAM</a>
+          <a href="#" onClick={()=>{
+            setContent(1);
+            toggle();
+          }} id="event-dark">
+            UDGAM
+          </a>
+          <a href="#" onClick={()=>{
+            setContent(2);
+            toggle();
+          }} id="event-dark2">
+            MASHAL
+          </a>
+        </div>
+      </tbody>
+    </table>
+    {/* <div>events</div>
         <div>
           <div></div>
         </div> */}
-      <EventCard data={content} />
-    </div>
+        <EventCard data={content}/>
+  </div>
   );
 }
 export default Event;
