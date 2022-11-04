b0d01b2431baa595358d0c414fa25c0cec8f12b8
Ankur Agrawal <ankuragrawal9455@gmail.com>
2022-11-04 19:10:59 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from './components/Home';
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { gapi } from 'gapi-script';
 
-const clientId = "868476725043-56q2l17h7bf2a1fpvkqp04t5br7mti4p.apps.googleusercontent.com"
+const clientId = process.env.GOOGLE_CLIENT_ID
 
 function App() {
   const [index,setIndex]=useState([]);
