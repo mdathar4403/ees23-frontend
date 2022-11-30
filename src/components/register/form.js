462b7f9088e5486e20939b707e01fce6a0e5c938
RasButAss <samarth3tankasali@gmail.com>
2022-11-30 18:18:21 +0530

    impoved responsiveness of registration page

--- src/components/register/form.js
+++ src/components/register/form.js
@@ -7,7 +7,7 @@ import { GoogleLogin } from 'react-google-login';
 import { useCallback } from 'react';
 import axios from 'axios';
 import { gapi } from "gapi-script"
-import logo from './ees_logo.png'
+import logo from '../images/eeslogored.png'
 import { useNavigate } from "react-router-dom";
 import AnimatedButton from '../../animated button/AnimatedButton';
 
@@ -19,7 +19,7 @@ export default function Form() {
   const navigate = useNavigate();
   const { register, handleSubmit, watch, formState: { errors } } = useForm()
   const onSubmit = data => console.log(data);
-  const [isGSignedIn,setisGSignedIn] = useState(0);
+  const [isGSignedIn, setisGSignedIn] = useState(0);
 
   const profileData = window.sessionStorage.getItem("profileData");
   const token = window.sessionStorage.getItem("tokenId");
@@ -42,7 +42,7 @@ export default function Form() {
     axios.post("http://eeswebsite.herokuapp.com/auth/google-login", header, data).then((resp) => {
       console.log(resp);
     });
-    window.sessionStorage.setItem("registered_email:"+profdata.email,1);
+    window.sessionStorage.setItem("registered_email:" + profdata.email, 1);
     navigate("/");
   }
   const onGoogleLoginSuccess = (res) => {
@@ -60,125 +60,123 @@ export default function Form() {
   };
 
 
-  useEffect(()=>{
+  useEffect(() => {
 
-  },[isGSignedIn]);
+  }, [isGSignedIn]);
 
   return (
     <>
-      {isGSignedIn === 0 && (
-        <>
-          <div className="g-sign-in-button">
-            <div className="register-button">
-              <GoogleLogin
-                accessType="online"
-                disabled={false}
-                client_id={clientId} // your Google app client ID
-                buttonText="Sign in with Google"
-                onSuccess={onGoogleLoginSuccess} // perform your user logic here
-                onFailure={onGoogleLoginFailure} // handle errors here
-                cookiePolicy={"single-host-origin"}
-                isSignedIn={true}
-                scope={scope}
-              />
+      <div className='bg'>
+      <div class="star-field">
+        <div class="layer"></div>
+        <div class="layer"></div>
+        <div class="layer"></div>
+        {isGSignedIn === 0 && (
+          <>
+            <div className="g-sign-in-button">
+              <div className="register-button">
+                <GoogleLogin
+                  accessType="online"
+                  disabled={false}
+                  client_id={clientId} // your Google app client ID
+                  buttonText="Sign in with Google"
+                  onSuccess={onGoogleLoginSuccess} // perform your user logic here
+                  onFailure={onGoogleLoginFailure} // handle errors here
+                  cookiePolicy={"single-host-origin"}
+                  isSignedIn={true}
+                  scope={scope}
+                />
+              </div>
+            </div>
+          </>
+        )}
+
+        {isGSignedIn == 1 && (
+          <div className="refisterForm">
+            <div className="register">
+              <div className="col-1">
+                <h1>
+                  <strong className="register-heading">Register</strong>
+                </h1>
+
+                <form
+                  id="form"
+                  className="flex flex-col"
+                  onSubmit={handleSubmit((data) => postData(data))}
+                >
+
+                  <input
+                    type="text"
+                    {...register("name")}
+                    value={profileData.givenName}
+                    placeholder={profileData.givenName}
+                    readOnly
+                  />
+                  
+                  <input
+                    type="text"
+                    {...register("email")}
+                    value={profileData.email}
+                    placeholder={profileData.email}
+                    readOnly
+                  />
+                  <input
+                    type="text"
+                    {...register("phone_number", { valueAsNumber: true })}
+                    placeholder="Whatsapp Number"
+                    required
+                  />
+                  <input
+                    type="text"
+                    {...register("college_name")}
+                    placeholder="College/Institute"
+                    required
+                  />
+                  <input
+                    list="years"
+                    {...register("year")}
+                    placeholder="Year"
+                    required
+                  />
+                  <datalist id="years">
+                    <option value="ONE">ONE</option>
+                    <option value="TWO">TWO</option>
+                    <option value="THIRD">Third</option>
+                    <option value="FOURTH">Fourth</option>
+                    <option value="FIFTH">Fifth</option>
+                  </datalist>
+                  <input
+                    type="text"
+                    {...register("branch")}
+                    placeholder="Branch"
+                    required
+                  />
+                  {errors.mobile?.type === "required" &&
+                    "Some Fields are missing!!!"}
+                  {errors.college_name?.type === "required" &&
+                    "College Name is required"}
+                  {errors.mobile?.type === "valueAsNumber" &&
+                    "Enter correct mobile Number"}
+                  {errors.year?.type === "required" && "Year is required"}
+                  {errors.branch?.type === "required" && "Branch is required"}
+                  {errors.Password?.type === "required" && "password is required"}
+                  {errors.ConfirmPassword?.type === "required" &&
+                    "confirm password is required"}
+                  <AnimatedButton
+                    className="signinbtn"
+                    text={"Register"}
+                  />
+                </form>
+              </div>
+              <div className="col-2">
+                <img className="formimg" src={logo} alt="" />
+              </div>
             </div>
           </div>
-        </>
-      )}
-
-      {isGSignedIn == 1 && (
-        <div className="refisterForm">
-          <div className="register">
-            <div className="col-1">
-              <h1>
-                <strong className="register-heading">Register</strong>
-              </h1>
-
-              <form
-                id="form"
-                className="flex flex-col"
-                onSubmit={handleSubmit((data) => postData(data))}
-              >
-                <input
-                  type="text"
-                  {...register("name")}
-                  value={profileData.givenName}
-                  placeholder={profileData.givenName}
-                  readOnly
-                />
-                <input
-                  type="text"
-                  {...register("email")}
-                  value={profileData.email}
-                  placeholder={profileData.email}
-                  readOnly
-                />
-                <input
-                  type="text"
-                  {...register("phone_number", { valueAsNumber: true })}
-                  placeholder="Whatsapp Number"
-                  required
-                />
-                <input
-                  type="text"
-                  {...register("college_name")}
-                  placeholder="College/Institute"
-                  required
-                />
-                <input
-                  list="years"
-                  {...register("year")}
-                  placeholder="Year"
-                  required
-                />
-                <datalist id="years">
-                  <option value="ONE">ONE</option>
-                  <option value="TWO">TWO</option>
-                  <option value="THIRD">Third</option>
-                  <option value="FOURTH">Fourth</option>
-                  <option value="FIFTH">Fifth</option>
-                </datalist>
-                <input
-                  type="text"
-                  {...register("branch")}
-                  placeholder="Branch"
-                  required
-                />
-                <input
-                  type="password"
-                  {...register("Password")}
-                  placeholder="Password"
-                  required
-                />
-                <input
-                  type="password"
-                  {...register("ConfirmPassword")}
-                  placeholder="Confirm Password"
-                  required
-                />
-                {errors.mobile?.type === "required" &&
-                  "Some Fields are missing!!!"}
-                {errors.college_name?.type === "required" &&
-                  "College Name is required"}
-                {errors.mobile?.type === "valueAsNumber" &&
-                  "Enter correct mobile Number"}
-                {errors.year?.type === "required" && "Year is required"}
-                {errors.branch?.type === "required" && "Branch is required"}
-                {errors.Password?.type === "required" && "password is required"}
-                {errors.ConfirmPassword?.type === "required" &&
-                  "confirm password is required"}
-                <AnimatedButton
-                  className="signinbtn"
-                  text={"Register"}
-                />
-              </form>
-            </div>
-            <div className="col-2">
-              <img className="formimg" src={logo} alt="" />
-            </div>
-          </div>
+        
+        )}
         </div>
-      )}
-    </>
-  );
+        </div>
+      </>
+      );
 }
