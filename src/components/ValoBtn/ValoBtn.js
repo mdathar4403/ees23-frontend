3d2b4e0995644b3f52052c0ef2e1ccf5cf4588e1
mdathar <mdathar4403@gmail.com>
2022-12-16 17:37:00 +0530

    valo-reg

--- src/components/ValoBtn/ValoBtn.js
+++ src/components/ValoBtn/ValoBtn.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import './ValoBtn.css'
+import valo from './images/valorant.png'
 
 const ValoBtn = () => {
-  return (
-    <div>
-       {/* Valo Button */}
-       <div className="valo-btn">
-          <button class="btn">
-            <span class="btn__inner">
-              <span class="btn__slide"></span>
-              <span class="btn__content">Register</span>
-            </span>
-          </button>
+    return (
+        <div>
+            {/* Valo Button */}
+
+            <div className="valo-btn">
+                <button class="btn btn--light">
+                    <span class="btn__inner">
+                        <span class="btn__slide"></span>
+                        <span class="btn__content">
+                            <img src={valo} className="img-valo" alt="valo" />
+                            Registration Opens
+                        </span>
+
+                    </span>
+                </button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ValoBtn
