914cdfe53adcd02fe43ecbaedda2ae9c4f473566
athar <md.athar.ece21@itbhu.ac.in>
2023-03-27 00:40:57 +0530

    mashal-event

--- src/Mashal/components/MashalEvent/mashalEvent.js
+++ src/Mashal/components/MashalEvent/mashalEvent.js
@@ -5,15 +5,15 @@ import './mashalEvent.css';
 // import img3 from './images/img3.jpg';
 // import img4 from './images/img4.jpg';
 import basket from './images/basket.jpg';
-import badminton from './images/badminton.jpg';
+import badminton from './images/Badminton.jpg';
 import football from './images/football.jpg';
 import chess from './images/chess.jpg';
-import Run from './images/Run.jpg';
+import Run from './images/run.jpg';
 import tennis from './images/tennis.jpg';
 import images from './images/images.jpg';
 import cycle from './images/cycle.jpg';
 import tt from './images/tt.jpg';
-import cricket from './images/cricket.jpg';
+import cricket from './images/Cricket.jpg';
 import vollyball from './images/vollyball.jpg';
 import athe from './images/athe.jpg';
 import tug from './images/tug.jpg';
@@ -24,7 +24,7 @@ import { BsTwitter } from 'react-icons/bs';
 import { BsGlobe } from 'react-icons/bs';
 import lg from './images/mashalLogo.svg';
 import Mashnav from '../Mashnav/mashnav';
-import InstagramEmbed from 'react-instagram-embed';
+// import InstagramEmbed from 'react-instagram-embed';
 
 const MashalEvent = () => {
   return (
@@ -32,7 +32,8 @@ const MashalEvent = () => {
       <div className="mashnavxxx">
         <Mashnav></Mashnav>
       </div>
-      <InstagramEmbed url="https://instagr.am/p/Zw9o4/" maxWidth={500} hideCaption={true} containerTagName="div" protocol="" injectScript onLoading={() => {}} onSuccess={() => {}} onAfterRender={() => {}} onFailure={() => {}} />
+      {/* <InstagramEmbed url="https://instagr.am/p/Zw9o4/" clientAccessToken="123|456" maxWidth={320} hideCaption={false} containerTagName="div" protocol="" injectScript onLoading={() => {}} onSuccess={() => {}} onAfterRender={() => {}} onFailure={() => {}} /> */}
+      {/* <InstagramEmbed url="https://instagr.am/p/Zw9o4/" maxWidth={500} hideCaption={true} containerTagName="div" protocol="" injectScript onLoading={() => {}} onSuccess={() => {}} onAfterRender={() => {}} onFailure={() => {}} /> */}
       <div className="event-section">
         <div className="heading-mashal">
           <h1>EVENTS</h1>
@@ -42,12 +43,52 @@ const MashalEvent = () => {
           <div className="mashal-card">
             <div className="card">
               <figure className="card__thumb">
+                <img src={cycle} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">
+                    CYCLING <span id="livenow">Live</span>
+                  </h2>
+                  <p className="card__snippet">Duration: 5-day event, Participation: Individual , Categories: Men’s and Women’s, Platform: Strava</p>
+                  <a href="https://www.instagram.com/p/Cocp6AgMuaH/?utm_source=ig_web_copy_link" className="card__button">
+                    See Post
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={tt} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">
+                    TABLE TENNIS <span id="livenow">Live</span>
+                  </h2>
+                  <p className="card__snippet">Team Size: 3 (for boys) , 2 (for girls), Matches: 3 singles (for boys), 2 singles, 1 doubles (for girls)</p>
+                  <a href="https://www.instagram.com/p/Co0FmG0M6b7/?utm_source=ig_web_copy_link" className="card__button">
+                    See Post
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={cricket} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">CRICKET</h2>
+                  <p className="card__snippet">Team Size: 11 players, Maximum Registered players: 16, Match: 10 - 10 overs, Format: ICC Rules </p>
+                  <a href="https://www.instagram.com/p/CqCi2T_oqcR/?utm_source=ig_web_copy_link" className="card__button">
+                    See Post
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
                 <img src={badminton} alt="Picture by Kyle Cottrell" className="card__image" />
                 <figcaption className="card__caption">
                   <h2 className="card__title">BADMINTON</h2>
                   <p className="card__snippet">Match: Singles Only Best of 3 Games:Knockouts,Quarter Finals,Semi-Finals,Finals</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
+                  <a href="https://www.instagram.com/p/CmDq6ZtM7Sl/?utm_source=ig_web_copy_link" className="card__button">
+                    See Post
                   </a>
                 </figcaption>
               </figure>
@@ -60,8 +101,8 @@ const MashalEvent = () => {
                     5-KM RUN <span id="livenow">Live</span>
                   </h2>
                   <p className="card__snippet">Track: 5 km running track Track: 5 km running track, Inside IIT BHU Campus</p>
-                  <a href="https://docs.google.com/forms/d/1tzb4yvLkVRlJsKWVUhtCMXnle894ImQMZL5lGfF17j0/viewform?edit_requested=true" className="card__button">
-                    Register
+                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSf7m0oJo7SCBTkHmU0aCcXrClotG7FdLBswknxI7tOwm6sNaA/closedform" className="card__button">
+                    See Post
                   </a>
                 </figcaption>
               </figure>
@@ -74,20 +115,20 @@ const MashalEvent = () => {
                     CHESS <span id="livenow">Live</span>
                   </h2>
                   <p className="card__snippet">Participation: Individual , 3 Rounds: Lichess Arena, Swiss Tournament, Offline Boards</p>
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSf7m0oJo7SCBTkHmU0aCcXrClotG7FdLBswknxI7tOwm6sNaA/closedform" target="__blank" className="card__button">
-                    Register
+                  <a href="https://www.instagram.com/p/CoPrkRGsjK9/?utm_source=ig_web_copy_link" target="__blank" className="card__button">
+                    See Post
                   </a>
                 </figcaption>
               </figure>
             </div>
             <div className="card">
               <figure className="card__thumb">
-                <img src={basket} alt="Picture by Kyle Cottrell" className="card__image" />
+                <img src={images} alt="Picture by Kyle Cottrell" className="card__image" />
                 <figcaption className="card__caption">
-                  <h2 className="card__title">BASKETBALL</h2>
-                  <p className="card__snippet">Team Size: 7 Players , Match:10mins x 4 quarters</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
+                  <h2 className="card__title">KABADDI</h2>
+                  <p className="card__snippet">Team Size: 10-14 Players, 7 Players on the field, rest extra, Match:20mins x 2 halves</p>
+                  <a href="https://www.instagram.com/p/CoQNqKzsg06/?utm_source=ig_web_copy_link" className="card__button">
+                    See Post
                   </a>
                 </figcaption>
               </figure>
@@ -100,8 +141,20 @@ const MashalEvent = () => {
                     KHO-KHO <span id="livenow">Live</span>
                   </h2>
                   <p className="card__snippet">Team Size: 12 players , Only 9 players take the field, Match: 9 mins x 2 innings</p>
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSciFs1FM-9L0ASPrTIsYNcHrngih1XivQ5M8WFO5ORzOML8DA/viewform" target="__blank" className="card__button">
-                    Register
+                  <a href="https://www.instagram.com/p/CoJlE4FB2VD/?utm_source=ig_web_copy_link" target="__blank" className="card__button">
+                    See Post
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={basket} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">BASKETBALL</h2>
+                  <p className="card__snippet">Team Size: 7 Players , Match:10mins x 4 quarters</p>
+                  <a href="#" className="card__button">
+                    Coming Soon
                   </a>
                 </figcaption>
               </figure>
@@ -130,58 +183,7 @@ const MashalEvent = () => {
                 </figcaption>
               </figure>
             </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={images} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">KABADDI</h2>
-                  <p className="card__snippet">Team Size: 10-14 Players, 7 Players on the field, rest extra, Match:20mins x 2 halves</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={cycle} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">
-                    CYCLING <span id="livenow">Live</span>
-                  </h2>
-                  <p className="card__snippet">Duration: 5-day event, Participation: Individual , Categories: Men’s and Women’s, Platform: Strava</p>
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSfV77ZaVMmLT5INZQbcg6NJAfPJDaDMqfWAmisxqOWv7CPgKg/viewform" className="card__button">
-                    Register
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={tt} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">
-                    TABLE TENNIS <span id="livenow">Live</span>
-                  </h2>
-                  <p className="card__snippet">Team Size: 3 (for boys) , 2 (for girls), Matches: 3 singles (for boys), 2 singles, 1 doubles (for girls)</p>
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSclYUMm1gN-VumVBwqM80p1WG80SMlJvw3nlq2N5YTIRJDcbg/viewform" className="card__button">
-                    Register
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={cricket} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">CRICKET</h2>
-                  <p className="card__snippet">Team Size: 11 players, Maximum Registered players: 16, Match: 10 - 10 overs, Format: ICC Rules </p>
-                  <a href="#" className="card__button">
-                    Coming Soon
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
+
             <div className="card">
               <figure className="card__thumb">
                 <img src={vollyball} alt="Picture by Kyle Cottrell" className="card__image" />
