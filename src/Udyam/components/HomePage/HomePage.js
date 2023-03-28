265d365ebc9dc8f388b4d53f1f481021e6af9098
RasButAss <samarth3tankasali@gmail.com>
2023-03-28 13:18:14 +0530

    globe issue resolved

--- src/Udyam/components/HomePage/HomePage.js
+++ src/Udyam/components/HomePage/HomePage.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './HomePage.css';
-import Globe from '../globe/Globe';
+import globe from './globe.mp4';
 
 function HomePage() {
   return (
     <div className="main-container">
+      {/* <img className="globegif" src={globe} /> */}
+      <video src={globe} type="video/mp4" autoPlay muted loop className="globegif"></video>
       <h1 className="heading">
         Udyam
         <h5 className="tag-line">Connecting techies around the globe ...</h5>
       </h1>
-      <Globe />
+      {/* <Globe /> */}
     </div>
   );
 }
