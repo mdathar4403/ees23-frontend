f9a4fa568dbc7f6a101f4ba69023ac85015ee201
athar <mdathar4403@gmail.com>
2023-03-30 00:17:09 +0530

    navbar-bg

--- src/Mashal/components/MashalEvent/mashalEvent.js
+++ src/Mashal/components/MashalEvent/mashalEvent.js
@@ -43,11 +43,94 @@ const MashalEvent = () => {
           <div className="mashal-card">
             <div className="card">
               <figure className="card__thumb">
-                <img src={cycle} alt="Picture by Kyle Cottrell" className="card__image" />
+                <img src={basket} alt="Picture by Kyle Cottrell" className="card__image" />
                 <figcaption className="card__caption">
                   <h2 className="card__title">
-                    CYCLING <span id="livenow">Live</span>
+                    BASKETBALL <span id="livenow">Live</span>
                   </h2>
+                  <p className="card__snippet">Team Size: 7 Players , Match:10mins x 4 quarters</p>
+                  <a href="#" className="card__button">
+                    Coming Soon
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={football} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">
+                    FOOTBALL <span id="livenow">Live</span>
+                  </h2>
+                  <p className="card__snippet">Team Size: 7 players, Maximum registered players: 10, Match: 15 mins x 2, Rolling Substitutions allowed</p>
+                  <a href="#" className="card__button">
+                    Coming Soon
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={tennis} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">
+                    LAWN TENNIS <span id="livenow">Live</span>
+                  </h2>
+                  <p className="card__snippet">Match : Singles Only, Round Robin / Knockout, Format: AITA Rules</p>
+                  <a href="#" className="card__button">
+                    Coming Soon
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={vollyball} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">
+                    VOLLEYBALL <span id="livenow">Live</span>
+                  </h2>
+                  <p className="card__snippet">Team Size: 6 - 12 players, Match: Best of 3 sets x 15 points, Finals: Best of 5 sets x 15 points</p>
+                  <a href="#" className="card__button">
+                    Coming Soon
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={athe} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">
+                    ATHLETICS <span id="livenow">Live</span>
+                  </h2>
+                  <p className="card__snippet">Races: 100m, 200m, 400m, Jumps: Long Jump, High Jump, Shot-Put</p>
+                  <a href="#" className="card__button">
+                    Coming Soon
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={tug} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">
+                    TUG OF WAR <span id="livenow">Live</span>
+                  </h2>
+                  <p className="card__snippet">Team Size: 8 players, Match: 3 pulls each match</p>
+                  <a href="#" className="card__button">
+                    Coming Soon
+                  </a>
+                </figcaption>
+              </figure>
+            </div>
+            <div className="card">
+              <figure className="card__thumb">
+                <img src={cycle} alt="Picture by Kyle Cottrell" className="card__image" />
+                <figcaption className="card__caption">
+                  <h2 className="card__title">CYCLING</h2>
                   <p className="card__snippet">Duration: 5-day event, Participation: Individual , Categories: Men’s and Women’s, Platform: Strava</p>
                   <a href="https://www.instagram.com/p/Cocp6AgMuaH/?utm_source=ig_web_copy_link" className="card__button">
                     See Post
@@ -59,9 +142,7 @@ const MashalEvent = () => {
               <figure className="card__thumb">
                 <img src={tt} alt="Picture by Kyle Cottrell" className="card__image" />
                 <figcaption className="card__caption">
-                  <h2 className="card__title">
-                    TABLE TENNIS <span id="livenow">Live</span>
-                  </h2>
+                  <h2 className="card__title">TABLE TENNIS</h2>
                   <p className="card__snippet">Team Size: 3 (for boys) , 2 (for girls), Matches: 3 singles (for boys), 2 singles, 1 doubles (for girls)</p>
                   <a href="https://www.instagram.com/p/Co0FmG0M6b7/?utm_source=ig_web_copy_link" className="card__button">
                     See Post
@@ -97,9 +178,7 @@ const MashalEvent = () => {
               <figure className="card__thumb">
                 <img src={Run} alt="Picture by Kyle Cottrell" className="card__image" />
                 <figcaption className="card__caption">
-                  <h2 className="card__title">
-                    5-KM RUN <span id="livenow">Live</span>
-                  </h2>
+                  <h2 className="card__title">5-KM RUN</h2>
                   <p className="card__snippet">Track: 5 km running track Track: 5 km running track, Inside IIT BHU Campus</p>
                   <a href="https://docs.google.com/forms/d/e/1FAIpQLSf7m0oJo7SCBTkHmU0aCcXrClotG7FdLBswknxI7tOwm6sNaA/closedform" className="card__button">
                     See Post
@@ -111,9 +190,7 @@ const MashalEvent = () => {
               <figure className="card__thumb">
                 <img src={chess} alt="Picture by Kyle Cottrell" className="card__image" />
                 <figcaption className="card__caption">
-                  <h2 className="card__title">
-                    CHESS <span id="livenow">Live</span>
-                  </h2>
+                  <h2 className="card__title">CHESS</h2>
                   <p className="card__snippet">Participation: Individual , 3 Rounds: Lichess Arena, Swiss Tournament, Offline Boards</p>
                   <a href="https://www.instagram.com/p/CoPrkRGsjK9/?utm_source=ig_web_copy_link" target="__blank" className="card__button">
                     See Post
@@ -137,85 +214,10 @@ const MashalEvent = () => {
               <figure className="card__thumb">
                 <img src={kho} alt="Picture by Kyle Cottrell" className="card__image" />
                 <figcaption className="card__caption">
-                  <h2 className="card__title">
-                    KHO-KHO <span id="livenow">Live</span>
-                  </h2>
+                  <h2 className="card__title">KHO-KHO</h2>
                   <p className="card__snippet">Team Size: 12 players , Only 9 players take the field, Match: 9 mins x 2 innings</p>
                   <a href="https://www.instagram.com/p/CoJlE4FB2VD/?utm_source=ig_web_copy_link" target="__blank" className="card__button">
                     See Post
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={basket} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">BASKETBALL</h2>
-                  <p className="card__snippet">Team Size: 7 Players , Match:10mins x 4 quarters</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={football} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">FOOTBALL</h2>
-                  <p className="card__snippet">Team Size: 7 players, Maximum registered players: 10, Match: 15 mins x 2, Rolling Substitutions allowed</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={tennis} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">LAWN TENNIS</h2>
-                  <p className="card__snippet">Match : Singles Only, Round Robin / Knockout, Format: AITA Rules</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={vollyball} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">VOLLEYBALL</h2>
-                  <p className="card__snippet">Team Size: 6 - 12 players, Match: Best of 3 sets x 15 points, Finals: Best of 5 sets x 15 points</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={athe} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">ATHLETICS</h2>
-                  <p className="card__snippet">Races: 100m, 200m, 400m, Jumps: Long Jump, High Jump, Shot-Put</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
-                  </a>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="card">
-              <figure className="card__thumb">
-                <img src={tug} alt="Picture by Kyle Cottrell" className="card__image" />
-                <figcaption className="card__caption">
-                  <h2 className="card__title">TUG OF WAR</h2>
-                  <p className="card__snippet">Team Size: 8 players, Match: 3 pulls each match</p>
-                  <a href="#" className="card__button">
-                    Coming Soon
                   </a>
                 </figcaption>
               </figure>
